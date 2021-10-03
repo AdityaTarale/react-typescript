04984c526acb7bffaea43675b569587d7a658872
adityatarale <tarale.adi@gmail.com>
2021-10-03 18:42:40 +0530

    10-useState-Type-Assertion
    changing type using 'as' keyword

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -6,7 +6,7 @@ type AuthUser = {
 };
 
 export const User = () => {
-  const [user, setUser] = useState<AuthUser | null>(null); // explicit type
+  const [user, setUser] = useState<AuthUser>({} as AuthUser); // type assertion , allow us to access name and email without optional chaining
 
   const handleLogin = () => {
     setUser({
@@ -14,15 +14,22 @@ export const User = () => {
       email: "johndoe@example.com",
     });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
+
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User name is {user?.name}</div>
-      <div>User email is {user?.email}</div>
+      <div>User name is {user.name}</div>
+      <div>User email is {user.email}</div>
     </div>
   );
 };
+
+/**
+ * !Note
+ * when you know the value will be set soon after component mount, you can use type assertion
+ * 
+ * ? Here we are lies to typescript the empty object {} is of type AuthUser
+ * when we know value will be set soon after component mount, and always have a value after we can use type assertion.
+ * if not then use optional value for useState hoook i.e useState<AuthUser | null>
+ *
+ */
