a50459e54119102932485c536b6546e6cea8e0f4
adityatarale <tarale.adi@gmail.com>
2021-10-03 19:23:09 +0530

    12-useReducer-Strict-Action-Types
    String literals and Discriminated union type

--- src/components/state/Counter.tsx
+++ src/components/state/Counter.tsx
@@ -4,19 +4,31 @@ type CounterState = {
   count: number;
 };
 
-type CounterAction = {
-  type: string;
+//* Discriminated Union
+
+// Recommended approach to typing useReducer function
+
+type UpdateAction = {
+  type: "increment" | "decrement" | "reset";
   payload: number;
 };
+
+type ResetAction = {
+  type: "reset";
+};
+
+type CounterAction = UpdateAction | ResetAction;
 
 const initialState = { count: 0 };
 
 function reducer(state: CounterState, action: CounterAction) {
   switch (action.type) {
     case "increment":
-      return { count: state.count + 1 };
+      return { count: state.count + action.payload };
     case "decrement":
-      return { count: state.count - 1 };
+      return { count: state.count - action.payload };
+    case "reset":
+      return initialState;
     default:
       return state;
   }
@@ -33,6 +45,7 @@ export const Counter = () => {
       <button onClick={() => dispatch({ type: "decrement", payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
 };
