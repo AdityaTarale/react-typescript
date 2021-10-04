276780266535909b22b99a622b2dca2e3b8741fc
adityatarale <tarale.adi@gmail.com>
2021-10-04 11:51:00 +0530

    21-Wrapping-HTML-Elements
    Omitting types

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
-import { Toast } from "./components/templateliterals/Toast";
+import { CustomButton } from "./components/html/Button";
+import { CustomInput } from "./components/html/Input";
 
 function App() {
   return (
     <div className="App">
-      <Toast position="center" />
+      <CustomButton
+        variant="primary"
+        type="submit"
+        onClick={() => console.log("Clicked")}
+      >
+        Primary Button
+      </CustomButton>
+      <CustomInput />
     </div>
   );
 }
